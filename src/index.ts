240f7e8d14d32b37e12e8fc5402f85c90c32f15b
JamieS1211 <jamieshepherd1211@gmail.com>
2020-08-13 22:00:53 +0100

    Add "data" type message test

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,12 @@ admin.initializeApp({
     databaseURL: "https://notification-demo-app-2600c.firebaseio.com"
 })
 
-app.get("/notificationTest", (req, res) => {
-    const deviceToken =
-        // eslint-disable-next-line max-len
-        "cX4kwZOSTMymJvANzj4KSa:APA91bEVg18PdxiqV6x6p8iHGvXtOw-L-0Rx6XL1ShLSCRDCwy-RjM5vs2RAQZHVHZPndkza0YPNFrc0j7CxdC_IA29uCe0MskEGXnC82E0hSGcZo-_729EhMlT4G6gUhfJ2H6eirSJv"
+const deviceToken =
+    // eslint-disable-next-line max-len
+    "cX4kwZOSTMymJvANzj4KSa:APA91bEVg18PdxiqV6x6p8iHGvXtOw-L-0Rx6XL1ShLSCRDCwy-RjM5vs2RAQZHVHZPndkza0YPNFrc0j7CxdC_IA29uCe0MskEGXnC82E0hSGcZo-_729EhMlT4G6gUhfJ2H6eirSJv"
 
+// See docs for info on "notification" type message - https://firebase.google.com/docs/cloud-messaging/concept-options#notifications
+app.get("/FCMTestNotificationMessage", (req, res) => {
     admin.messaging().send({
         token: deviceToken,
         notification: {
@@ -23,6 +24,18 @@ app.get("/notificationTest", (req, res) => {
             body:
                 // eslint-disable-next-line max-len
                 "2 Notifications, one with and one without logo, will arrive. Clicking these notifications does not run NotificationResponseRecievedListner SDK30.0.9"
+        }
+    })
+
+    res.send("done")
+})
+
+// See docs for info on "data" type messages - https://firebase.google.com/docs/cloud-messaging/concept-options#data_messages
+app.get("/FCMTestDataMessage", (req, res) => {
+    admin.messaging().send({
+        token: deviceToken,
+        data: {
+            key: "Value"
         }
     })
 
